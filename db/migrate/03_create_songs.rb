ef2b3e7cc3dfe66e39dde275311d4de55ebc2291
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-11-30 20:04:23 +0000

    Done.

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,10 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
+  def change 
+    create_table :songs do |t|
+      t.string :name, null: false 
+      t.integer :artist_id
+      t.integer :genre_id
+      t.timestamps
+    end  
+  end
 end
